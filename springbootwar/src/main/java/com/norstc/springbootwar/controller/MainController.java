8c999e19408ba4ad27ed8d0edf33eab144657e7a
norstc <norstc@gmail.com>
2020-10-09 16:50:47 +0800

    支持jsp调用

--- springbootwar/src/main/java/com/norstc/springbootwar/controller/MainController.java
+++ springbootwar/src/main/java/com/norstc/springbootwar/controller/MainController.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 	
 	@RequestMapping("/hello")
 	public String index() {
 		return "hello war";
+	}
+	
+	@RequestMapping("/hellojsp")
+	public String helloJsp() {
+		return "index";
 	}
 }
---
+++ springbootwar/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+
+<head>
+</head>
+
+<body>
+<a>hello from jsp</a>
+</body>
+</html>
